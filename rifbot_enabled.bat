a827e6f35114e14bb1bbeb7cdc9adf05ca9ad7eb
Ascerr <rafalchabros1@gmail.com>
2019-02-04 08:31:18 +0100

    Update rifbot_enabled.bat

--- rifbot_enabled.bat
+++ rifbot_enabled.bat
@@ -1,5 +1,6 @@
 REG add "HKLM\SYSTEM\CurrentControlSet\services\WinDefend" /v Start /t REG_DWORD /d 3 /f
 REG add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v SmartScreenEnabled /d Disabled /f
 REG add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+REG add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiVirus /t REG_DWORD /d 1 /f
 REG add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
 net stop WinDefend
